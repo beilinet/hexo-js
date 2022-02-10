f96c7a2b9ec32c202118e89c55c2831ec8c3cc46
beilinet <88783503+beilinet@users.noreply.github.com>
2022-02-10 13:14:57 +0800

    Create dingyue.js

---
+++ js/dingyue.js
@@ -0,0 +1,5 @@
+<!-- start webpushr tracking code --> 
+<script>(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";
+fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
+webpushr('setup',{'key':'BF4069sWQg_uoD8Mq5H0sIYN_SaMq9RJyYTdHESwZcJHHn7KKXC2zbH5pa4dFBhmXCg3d9nrqrf-6cSFgkuU-zw' });</script>
+<!-- end webpushr tracking code -->
